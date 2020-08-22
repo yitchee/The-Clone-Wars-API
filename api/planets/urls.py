d047d83832c3d3f956c2085c5271e77f60cd1ebf
Yit Chee Chin <yitchee.chin@gmail.com>
2020-08-22 03:15:17 +0100

    Added .env file
    - Set up 'ID' endpoint for planets.
    - Can search for characters via URL query parameters. (name, affiliation, region)

--- api/planets/urls.py
+++ api/planets/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='planets_index'),
     path('random', views.planets_random, name='planets_random'),
+    path('<int:id>/', views.planets_id, name='planets_id'),
 ]
