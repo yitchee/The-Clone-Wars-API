d4a3c7e19693335b8883a838fb14a67b920abd82
Yit Chee Chin <yitchee.chin@gmail.com>
2020-08-22 04:02:49 +0100

    - Set up 'ID' endpoint for species. - Can search for species via URL query parameters. (name, affiliation, region)

--- api/species/urls.py
+++ api/species/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='species_index'),
     path('random', views.species_random, name='species_random'),
+    path('<int:id>/', views.species_id, name='species_id'),
 ]
--- api/species/views.py
+++ api/species/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from django.conf import settings
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -9,8 +10,34 @@ from .models import Species
 from .serializers import SpeciesSerializer
 
 
+@api_view(['GET'])
 def index(request):
-    return HttpResponse('<h1>Species</h1>')
+    name = request.GET.get('name', None)
+    designation = request.GET.get('designation', None)
+    page = int(request.GET.get('page', 0))
+
+    species_set = Species.objects.all().order_by('id')
+    if name:
+        species_set = species_set.filter(name__icontains=name)
+    
+    if designation:
+        # Avaliable Options : 'Sentient', 'Non-Sentient', 'Semi-Sentient', 'Undefined'
+        species_set = species_set.filter(info__designation__iexact=designation)
+
+    if page:
+        start = settings.RESOURCE_LIMIT*(page-1)
+        end = settings.RESOURCE_LIMIT*(page-1)+settings.RESOURCE_LIMIT
+        species_set = species_set[start:end]
+    else:
+        species_set = species_set[0:settings.RESOURCE_LIMIT]
+    
+    serializer = SpeciesSerializer(species_set, many=True)
+
+    # If nothing matches queries
+    if not serializer.data:
+        return Response({"error": settings.MSG_404}, status=404)
+    
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -18,4 +45,15 @@ def species_random(request):
     species = Species.objects.all()
     random_species = random.choice(species)
     serializer = SpeciesSerializer(random_species, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def species_id(request, id):
+    try:
+        species = Species.objects.get(id=id)
+    except:
+        return Response({"error": settings.MSG_404}, status=404)
+        
+    serializer = SpeciesSerializer(species, many=False)
     return Response(serializer.data)
